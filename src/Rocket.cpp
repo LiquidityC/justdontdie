a2974d80ad940d22cd5254a6b03380625e85677c
Linus_Probert <linus.probert@netset.se>
2014-10-02 10:20:19 +0200

    Implemented rockets

---
+++ src/Rocket.cpp
@@ -0,0 +1,13 @@
+#include "Rocket.h"
+
+void Rocket::preRender(const flat2d::RenderData *data)
+{
+	flat2d::Camera *cam = data->getCamera();
+	SDL_Rect boundingBox = getBoundingBox();
+	if (!cam->isVisibleOnCamera(boundingBox)) {
+		setDead(true);
+		return;
+	}
+
+	xpos += (xvel * cam->getDeltaTime());
+}
---
+++ src/Rocket.h
@@ -0,0 +1,29 @@
+#ifndef _ROCKET_H
+#define _ROCKET_H
+
+#include <flat/flat.h>
+
+class Rocket : public flat2d::RenderedGameObject
+{
+	private:
+		bool ghostMode = false;
+		float xvel;
+
+	public:
+		Rocket(int x, int y, bool ghost = false, bool leftToRight = true) :
+			flat2d::RenderedGameObject(x, y, 32, 32),
+			ghostMode(ghost) {
+				xvel = leftToRight ? -300 : 300;
+				setCollidable(true);
+
+				int xclip = leftToRight ? 0 : 32;
+				int yclip = ghostMode ? 32 : 0;
+
+				SDL_Rect clip = { xclip, yclip, 32, 32 };
+				setClip(clip);
+			};
+
+		void preRender(const flat2d::RenderData*);
+};
+
+#endif
