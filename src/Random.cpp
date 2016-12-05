83051505ba320f3495c87da2b50a1ffda900db2e
Linus Probert <linus.probert@gmail.com>
2016-12-05 23:22:04 +0100

    Different random generator

--- src/Random.cpp
+++ src/Random.cpp
@@ -3,7 +3,7 @@
 int getRandomBetween(int a, int b)
 {
 	std::random_device rd;
-	std::mt19937 gen(rd());
+	std::minstd_rand gen(rd());
 	std::uniform_int_distribution<int> dis(a, b);
 	return dis(gen);
 }
